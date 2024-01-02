cfcb54d6292efee29a6e748d51fee3a72ed40eb6
Wevergtonhs <wevergton.hs@gmail.com>
2024-01-02 12:46:57 +0000

    First Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function imgSlider(anything) {
+    document.querySelector('.main-glass').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+
+    circle.style.background = color;
+}
